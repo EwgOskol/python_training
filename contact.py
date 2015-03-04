12746215751d95b9d8384d93476ab814e80f5aa1
EwgOskol <ewgenij_garbuz@relex.ru>
2015-03-04 17:09:24 +0300

    Сценарий для добавления контакта
    Выделены вспомогательные методы и классы

---
+++ contact.py
@@ -0,0 +1,14 @@
+__author__ = 'tester'
+
+
+class Contact:
+
+    def __init__(self, fname, lname, company, address, homephone, mobilephone, workphone, hm_page):
+         self.fname = fname
+         self.lname = lname
+         self.company = company
+         self.address = address
+         self.homephone = homephone
+         self.mobilephone = mobilephone
+         self.workphone = workphone
+         self.hm_page = hm_page
