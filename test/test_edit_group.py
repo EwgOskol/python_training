03c2d50d191c992d90bdd4e02709b4780b8e3af1
EwgOskol <ewgenij_garbuz@relex.ru>
2015-03-27 12:25:25 +0300

    Revert "Доработаны тесты для групп."
    This reverts commit 85bb689cf5ebe6aed07c0e4e756e0acf8eb12f9b.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,13 +2,11 @@ __author__ = 'tester'
 
 from model.group import Group
 
-
 def test_delete_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.delete_first_group()
-    assert len(old_groups) - 1 == app.group.count()
+    app.group.modify_first_group(Group(name="test_nm1", header="test_hdr1", footer="test_ftr1"))
     new_groups = app.group.get_group_list()
-    old_groups[0:1] = []
-    assert old_groups == new_groups
+    assert len(old_groups) == len(new_groups)
+
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,20 +1,18 @@
 __author__ = 'tester'
 
 from model.group import Group
-from random import randrange
 
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
     group = Group(name="New group")
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[index] = group
+    old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
